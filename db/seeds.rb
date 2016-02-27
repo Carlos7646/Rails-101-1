8c458a77c215d5bb43e2627a21b1e084145e2d2d
Carlos <j7604060206tw@yahoo.com.tw>
2016-02-28 02:37:48 +0800

    Chapter 10 Set seed.rb, dev:build empty and dev:rebuild with seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+puts "Hello World!!"
+puts "The seed will create an account and 20 groups which have 30 posts"
+
+create_account = User.create([email: "test_user@test.test", password: "test1234", password_confirmation: "test1234", name: "Test User"])
+
+create_groups = for i in 1..20 do
+                  Group.create!([title: "Group no.#{i}", description: "The #{i} group has been created by seed", user_id: 1])
+                  GroupUser.create(group_id: i, user_id: 1)
+                  for k in 1..30 do
+                    Post.create!([group_id: "#{i}", content: "The #{k} post create by seed!", user_id: 1])
+                  end
+                end
+
